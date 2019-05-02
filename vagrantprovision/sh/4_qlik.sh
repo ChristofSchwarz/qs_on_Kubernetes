50696f31924ce0f6cfa39a957cbd561f48ee4e03
Christof Schwarz <christof.schwarz@gmx.at>
2019-05-02 18:58:33 +0200

    Update 4_qlik.sh

--- vagrantprovision/sh/4_qlik.sh
+++ vagrantprovision/sh/4_qlik.sh
@@ -12,7 +12,7 @@ helm repo add qlik-stable https://qlik.bintray.com/stable
 helm repo add qlik-edge https://qlik.bintray.com/edge
 
 # install the initial settings
-helm install -n qsek8s-init qlik-stable/qliksense-init
+helm install -n qlikinit qlik-stable/qliksense-init
 
 # install qlik sense enterprise on kubernetes
 helm install -n qlik qlik-stable/qliksense -f /vagrant/files/qliksense.yaml 
