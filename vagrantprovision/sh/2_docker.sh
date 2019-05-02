da8f69d8e0c13de86b1f7c9a4eac76bc0aa4a06b
Christof Schwarz <christof.schwarz@gmx.at>
2019-05-02 16:25:59 +0200

    Update 2_docker.sh

--- vagrantprovision/sh/2_docker.sh
+++ vagrantprovision/sh/2_docker.sh
@@ -1,4 +1,4 @@
-echo 'executing "2_docker.sh"'
+echo 'executing "docker.sh"'
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
