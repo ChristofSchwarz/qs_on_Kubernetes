6d1a355b5b0a95000089eeac192fed4a8d4595e8
Christof Schwarz <christof.schwarz@gmx.at>
2019-05-02 14:57:06 +0200

    Update 4_qlik.sh

--- vagrantprovision/sh/4_qlik.sh
+++ vagrantprovision/sh/4_qlik.sh
@@ -1,2 +1,15 @@
 #Install storageClass on NFS provider
 helm install --name qmi stable/nfs-client-provisioner -f /vagrant/files/storageClass.yaml
+
+#Create Persistent Volume Claims
+kubectl apply -f /vagrant/files/qsefe/pvc.yaml
+
+#Install Mongo
+helm install --name qlikmongo -f /vagrant/files/qsefe/mongo-helm.yaml stable/mongodb
+
+
+#Install qliksense from stable repo
+helm repo add qlik-stable https://qlik.bintray.com/stable
+
+helm install --name qliksense-init qlik/qliksense-init
+helm install -n qsefe qlik-stable/qliksense -f /vagrant/files/qliksense.yaml 
