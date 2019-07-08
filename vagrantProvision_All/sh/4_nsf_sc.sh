c00fe2fcf56a7c8e1cce7c544702d969a0748827
Christof Schwarz <christof.schwarz@gmx.at>
2019-07-08 13:17:35 +0200

    Update 4_nsf_sc.sh

--- vagrantProvision_All/sh/4_nsf_sc.sh
+++ vagrantProvision_All/sh/4_nsf_sc.sh
@@ -1,4 +1,4 @@
-echo 'executing "4_nsf_pvc.sh"'
+echo 'executing "4_nsf_sc.sh"'
 
 #Install storageClass on NFS provider
 helm install -n nfs stable/nfs-client-provisioner -f /vagrant/files/storageClass.yaml
