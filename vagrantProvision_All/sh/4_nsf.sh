84a7d553f8c81bd4ccb3ad36154abac635826023
Christof Schwarz <christof.schwarz@gmx.at>
2019-07-08 13:20:29 +0200

    Update and rename 4_nsf_sc.sh to 4_nsf.sh

--- vagrantProvision_All/sh/4_nsf.sh
+++ vagrantProvision_All/sh/4_nsf.sh
@@ -1,7 +1,7 @@
-echo 'executing "4_nsf_sc.sh"'
+echo 'executing "4_nsf.sh"'
 
-#Install storageClass on NFS provider
+echo 'Installing storageClass on NFS provider'
 helm install -n nfs stable/nfs-client-provisioner -f /vagrant/files/storageClass.yaml
 
-#Install PVC on above storageClass
+echo 'Installing PVC on above storageClass'
 kubectl apply -f /vagrant/files/pvc_nfs.yaml
