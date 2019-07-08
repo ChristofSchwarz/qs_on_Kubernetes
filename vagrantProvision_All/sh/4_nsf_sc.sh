d4ed28fc38dcef643250059bdbafbd8bb6c34289
Christof Schwarz <christof.schwarz@gmx.at>
2019-07-08 13:17:14 +0200

    Update and rename 4_nsf_pvc.sh to 4_nsf_sc.sh

--- vagrantProvision_All/sh/4_nsf_sc.sh
+++ vagrantProvision_All/sh/4_nsf_sc.sh
@@ -2,6 +2,3 @@ echo 'executing "4_nsf_pvc.sh"'
 
 #Install storageClass on NFS provider
 helm install -n nfs stable/nfs-client-provisioner -f /vagrant/files/storageClass.yaml
-
-#Create Persistent Volume Claims
-kubectl apply -f /vagrant/files/pvc.yaml
