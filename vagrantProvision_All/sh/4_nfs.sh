945f670ed996bbb157d1e2732d4a43716f319df4
Christof Schwarz <christof.schwarz@gmx.at>
2019-07-08 14:56:18 +0200

    Update 4_nfs.sh

--- vagrantProvision_All/sh/4_nfs.sh
+++ vagrantProvision_All/sh/4_nfs.sh
@@ -1,6 +1,7 @@
 echo 'executing "4_nfs.sh"'
 
 echo 'Installing storageClass on NFS provider'
+helm repo update
 helm install -n nfs stable/nfs-client-provisioner -f /vagrant/files/storageClass.yaml
 
 echo 'Installing PVC on above storageClass'
