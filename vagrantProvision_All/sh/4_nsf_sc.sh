53c0fbf38af7cd081e9cf3bab1af90a9b0d633f3
Christof Schwarz <christof.schwarz@gmx.at>
2019-07-08 13:19:31 +0200

    Update 4_nsf_sc.sh

--- vagrantProvision_All/sh/4_nsf_sc.sh
+++ vagrantProvision_All/sh/4_nsf_sc.sh
@@ -2,3 +2,6 @@ echo 'executing "4_nsf_sc.sh"'
 
 #Install storageClass on NFS provider
 helm install -n nfs stable/nfs-client-provisioner -f /vagrant/files/storageClass.yaml
+
+#Install PVC on above storageClass
+kubectl apply -f /vagrant/files/pvc_nfs.yaml
