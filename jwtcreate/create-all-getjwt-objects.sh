638a3dec2ae93d58e7f3fb146d4438d85b7ca03f
Christof Schwarz <christof.schwarz@gmx.at>
2019-09-13 13:00:01 +0200

    move out jwtcreate

--- jwtcreate/create-all-getjwt-objects.sh
+++
@@ -1,3 +0,0 @@
-kubectl create secret generic qlikcustom-jwtkey --from-file priv.key
-kubectl create -f getjwt-pod.yaml
-kubectl create -f getjwt-svc-np.yaml
--- jwtcreate/remove-all-getjwt-objects.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete pod qlikcustom-getjwt-pod
-kubectl delete service qlikcustom-getjwt-np
-kubectl delete service qlikcustom-getjwt-lb
-kubectl delete secret qlikcustom-jwtkey
