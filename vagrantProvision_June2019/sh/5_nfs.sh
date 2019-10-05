906306372f57e748a04772f57288eb76f6c3a785
Christof Schwarz <christof.schwarz@gmx.at>
2019-10-05 12:02:21 +0200

    clean-up

--- vagrantProvision_June2019/sh/5_nfs.sh
+++
@@ -1,9 +0,0 @@
-echo 'executing "5_nfs.sh"'
-
-echo 'Installing storageClass on NFS provider'
-#helm init
-#helm repo update
-helm install -n nfs stable/nfs-client-provisioner -f /vagrant/yaml/storageClass.yaml
-
-echo 'Installing PVC on above storageClass'
-kubectl apply -f /vagrant/yaml/pvc_nfs.yaml
--- vagrantProvision_June2019/sh/6_mongo.sh
+++
@@ -1,9 +0,0 @@
-echo 'execute "6_mongo.sh"'
-
-echo 'Creating PVC for MongoDB'
-kubectl apply -f /vagrant/yaml/pvc_mongo.yaml
-
-echo 'Installing MongoDB chart'
-#helm init
-#helm repo update
-helm install -n mongo stable/mongodb -f /vagrant/yaml/mongo.yaml 
