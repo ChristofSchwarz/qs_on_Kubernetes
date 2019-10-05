4c6ddda0e64ed53d8fdbf2f957ab33e6df5ad87e
Christof Schwarz <christof.schwarz@gmx.at>
2019-10-05 12:03:10 +0200

    cleanup

--- keycloak/restart.sh
+++
@@ -1,3 +0,0 @@
-kubectl delete $(kubectl get pods -o=name|grep "nginx-ingress")
-kubectl delete $(kubectl get pods -o=name|grep "edge-auth")
-kubectl delete $(kubectl get pods -o=name|grep  "identity-providers")
